e29e02f9aacf146cd76fad0900883e3dcbfe3290
Niels van der Velden <n.s.j.vandervelden@gmail.com>
2024-11-20 10:16:36 -0400

    reflow of comments

--- R/NGLVieweR_example.R
+++ R/NGLVieweR_example.R
@@ -1,15 +1,15 @@
 #'
-#' Run NGLVieweR example Shiny app
+#'Run NGLVieweR example Shiny app
 #'
-#' @description
-#' Launch an example to demonstrate how to use \code{NGLvieweR} in Shiny.
+#'@description Launch an example to demonstrate how to use \code{NGLvieweR} in
+#'Shiny.
 #'
-#' @param example Example type for which to see an example, possible values are:
-#' "basic", "API", "addSelection", "removeSelection", "snapshot",
-#' "updateAnimation", "updateColor", "updateFocus", "updateFullscreen",
-#' "updateRepresentation", "updateSelection", "updateStage", "updateVisibility" and
-#' "updateZoomMove".
-#' @return Call to load Shiny example.
+#'@param example Example type for which to see an example, possible values are:
+#'  "basic", "API", "addSelection", "removeSelection", "snapshot",
+#'  "updateAnimation", "updateColor", "updateFocus", "updateFullscreen",
+#'  "updateRepresentation", "updateSelection", "updateStage", "updateVisibility"
+#'  and "updateZoomMove".
+#'@return Call to load Shiny example.
 #' @examples
 #'
 #' if (interactive()) {
